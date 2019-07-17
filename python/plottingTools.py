ad3b345cf1121be9470a4b69475aa94b45597312
Liam <liamwezenbeek@hotmail.com>
2019-07-17 12:27:29 +0200

    Add tool to upload plots to webpage

--- python/plottingTools.py
+++ python/plottingTools.py
@@ -1,15 +1,16 @@
 import ROOT
-from ROOT import TLine, THStack, TColor, TCanvas, TLegend, gROOT, gStyle, TH1, TMultiGraph, TGraphErrors, TGraph, TLatex, TPad
+from ROOT import TLine, THStack, TColor, TCanvas, TLegend, gROOT, gStyle, TH1, TMultiGraph, TGraphErrors, TGraph, TLatex, TPad, TMath
 import CMS_lumi as cl
 import tdrstyle as tdr
 import numpy as np
-from helpers import makeDirIfNeeded
+from helpers import makeDirIfNeeded, isTimeStampFormat, sortByOtherList
+import os
 
-def GeneralSettings():
+def GeneralSettings(paintformat = "4.2f"):
     gROOT.SetBatch(True)
     gStyle.SetOptStat(0)
     tdr.setTDRStyle()
-    gStyle.SetPaintTextFormat("4.2f")
+    gStyle.SetPaintTextFormat(paintformat)
     gROOT.ProcessLine( "gErrorIgnoreLevel = 1001;")
 
 def GetStackColor(index):
@@ -25,8 +26,16 @@ def GetStackColorTauPOG(index):
     if index == 1:      return "#de5a6a"
     if index == 2:      return "#9999cc"
     if index == 3:      return "#4496c8"
-    if index == 4:      return "#ffcc66"
-    if index == 5:      return "#e5b7e5"
+    if index == 4:      return "#e5b7e5"
+    if index == 5:      return "#ffcc66"
+
+def GetStackColorTauPOGbyName(name):
+    if 'VV' in name:            return "#18252a"
+    elif 'TT' in name:          return "#de5a6a"
+    elif 'ST' in name:          return "#9999cc"
+    elif 'WJets' in name:       return "#4496c8"
+    elif 'QCD' in name:         return "#e5b7e5"
+    elif 'DY' in name:          return "#ffcc66"
 
 def GetHistColor(index):        
     if index == 0:      return "#000000"
@@ -35,16 +44,24 @@ def GetHistColor(index):
     if index == 3:      return "#f58231"
     if index == 4:      return "#3cb44d"
     if index == 5:      return "#ffe119"
+    if index == 6:      return "#87F1FF"
+    if index == 7:      return "#F4F1BB"
 
 def GetLineColor(index):
     if index == 0:      return "#000000"
     if index == 1:      return "#e6194B"
     if index == 2:      return "#4363d8"
+    if index == 3:      return "#B9BAA3"
+    if index == 4:      return "#685762"
+    if index == 5:      return "#E8C547"
 
 def GetMarker(index):
     if index == 0:      return 20
     if index == 1:      return 21
     if index == 2:      return 22
+    if index == 3:      return 23
+    if index == 4:      return 24
+    if index == 5:      return 25
 
 def GetOverallMaximum(hist):
     currentMax = 0
@@ -52,10 +69,11 @@ def GetOverallMaximum(hist):
         if h.GetMaximum() > currentMax :    currentMax = h.GetMaximum()
     return currentMax
 
-def GetOverallMinimum(hist):
+def GetOverallMinimum(hist, zero_not_allowed = False):
     currentMin = 99999
-    for h in hist:
-        if h.GetMinimum() < currentMin :    currentMin = h.GetMinimum()
+    for i, h in enumerate(hist):
+        if h.GetMinimum() == -1 and zero_not_allowed: continue
+        if h.GetMinimum() < currentMin:    currentMin = h.GetMinimum()
     return currentMin
 
 def GetNestedMin(arr):
@@ -69,6 +87,40 @@ def GetNestedMax(arr):
     for a in arr:
         localMax.append(np.max(a))
     return np.max(localMax)
+
+def GetXMin(graphs):
+    xmin = 99999.
+    for graph in graphs:
+        if TMath.MinElement(graph.GetN(),graph.GetX()) < xmin:
+            xmin = TMath.MinElement(graph.GetN(),graph.GetX())
+    return xmin
+
+def GetXMax(graphs):
+    xmax = -99999.
+    for graph in graphs:
+        if TMath.MaxElement(graph.GetN(),graph.GetX()) > xmax:
+            xmax = TMath.MaxElement(graph.GetN(),graph.GetX())
+    return xmax
+
+def GetYMin(graphs):
+    ymin = 99999.
+    for graph in graphs:
+        if TMath.MinElement(graph.GetN(),graph.GetY()) < ymin:
+            ymin = TMath.MinElement(graph.GetN(),graph.GetY())
+    return ymin
+
+def GetYMax(graphs):
+    ymax = -99999.
+    for graph in graphs:
+        if TMath.MaxElement(graph.GetN(),graph.GetY()) > ymax:
+            ymax = TMath.MaxElement(graph.GetN(),graph.GetY())
+    return ymax
+
+def orderHist(hist, names, lowestFirst = False):
+    weight = -1.
+    if lowestFirst: weight = 1.
+    sof = [h.GetSumOfWeights()*weight for h in hist]
+    return sortByOtherList(hist, sof), sortByOtherList(names, sof)
 
 def extraTextFormat(text, xpos = None, ypos = None, textsize = None, align = 12):
     return [text, xpos, ypos, textsize, align]
@@ -119,8 +171,33 @@ def DrawExtraText(pad, additionalInformation):
         print 'Please provide the text to draw'
 
     pad.Update()
-    
-def plotDataVSMC(DataHist, MCHist, xtitle, legendNames, DataName, destination):
+
+def getUnit(x):
+    if x.find('[') == -1:
+        return ''
+    else:
+        return x[x.find('[')+len('['):x.rfind(']')]
+
+def savePlots(Canv, destination):
+    destination_components = destination.split('/')
+    cleaned_components = [x for x in destination_components if not isTimeStampFormat(x)]
+    index_for_php = cleaned_components.index('Results')
+    php_destination = '/user/lwezenbe/public_html/'
+    php_destination += '/'.join(cleaned_components[index_for_php+1:])
+    makeDirIfNeeded(php_destination.rsplit('/', 1)[0])    
+    os.system('cp -rf /user/lwezenbe/private/PhD/index.php '+ php_destination.rsplit('/', 1)[0]+'/index.php')    
+
+    print destination
+
+    Canv.SaveAs(destination + ".pdf")
+    Canv.SaveAs(destination + ".png")
+    Canv.SaveAs(destination + ".root")
+
+    Canv.SaveAs(php_destination + ".pdf")
+    Canv.SaveAs(php_destination + ".png")
+    Canv.SaveAs(php_destination + ".root")
+
+def plotDataVSMC(DataHist, MCHist, xtitle, legendNames, DataName, destination, ytitle_bottom = 'Data/MC'):
     GeneralSettings()
     
     Canv = TCanvas("Canv"+destination, "Canv"+destination, 1000, 1000)
@@ -186,14 +263,14 @@ def plotDataVSMC(DataHist, MCHist, xtitle, legendNames, DataName, destination):
     ratiopad.cd()
     
     #Set Style for bottom plot
-    DataOverMC.SetTitle(";" + xtitle + "; Data/MC")
+    DataOverMC.SetTitle(";" + xtitle + "; "+ytitle_bottom)
     DataOverMC.GetXaxis().SetTitleSize(.12)
     DataOverMC.GetYaxis().SetTitleSize(.12)
     DataOverMC.GetYaxis().SetTitleOffset(.6)
     DataOverMC.GetXaxis().SetLabelSize(.12)
     DataOverMC.GetYaxis().SetLabelSize(.12)
-    DataOverMC.SetMinimum(0.)
-    DataOverMC.SetMaximum(2.)
+    DataOverMC.SetMinimum(0.6)
+    DataOverMC.SetMaximum(1.4)
     DataOverMC.Draw("EP")
 
     #Draw a guide for the eye
@@ -205,11 +282,96 @@ def plotDataVSMC(DataHist, MCHist, xtitle, legendNames, DataName, destination):
    
     #Throw CMs lumi at it
     cl.CMS_lumi(Canv, 4, 11, 'Preliminary', True)
+    print 'save'
  
     #Save everything
-    Canv.SaveAs(destination + ".pdf")
-    Canv.SaveAs(destination + ".png")
-    Canv.SaveAs(destination + ".root")
+    savePlots(Canv, destination)
+    print 'saved'
+
+def plotClosure(observed, predicted, xtitle, ytitle, DataName, additionalInfo, destination, yLog = False):
+    GeneralSettings()
+    
+    Canv = TCanvas("Canv"+destination, "Canv"+destination, 1000, 1000)
+ 
+    observed.SetMarkerColor(ROOT.kBlack)
+    observed.SetLineColor(ROOT.kBlack)    
+    observed.SetMarkerStyle(20)    
+   
+    predicted.SetFillColor(TColor.GetColor('#3399ff'))
+    predicted.SetLineColor(TColor.GetColor('#3399ff'))
+ 
+    #First pad
+    plotpad = TPad("plotpad", "plotpad", 0, .3, 1, 0.98)
+    plotpad.SetBottomMargin(0.025)
+    plotpad.Draw()
+    plotpad.cd()
+
+    predicted.Draw("EHist")                                                            #Draw before using GetHistogram, see https://root-forum.cern.ch/t/thstack-gethistogram-null-pointer-error/12892/4
+    title = " ; ; "+ytitle+" / " +str(predicted.GetBinWidth(1)) +' '+ getUnit(xtitle)
+    predicted.SetTitle(title)
+    predicted.GetXaxis().SetLabelOffset(9999999)
+
+    #Set range
+    overallMin = GetOverallMinimum([observed, predicted], yLog)
+    overallMax = GetOverallMaximum([observed, predicted])
+
+    if yLog:
+        plotpad.SetLogy()
+        predicted.SetMinimum(0.3*overallMin)    
+        predicted.SetMaximum(10*overallMax)    
+    else:
+        predicted.SetMinimum(0.5*overallMin)    
+        predicted.SetMaximum(1.2*overallMax)    
+    observed.Draw("EPSame")
+     
+    #Create Legend
+    legend = TLegend(0.7, .7, .9, .9)
+    legend.AddEntry(observed, DataName + ' (observed)')  
+    legend.AddEntry(predicted, DataName + ' (predicted)')  
+    legend.SetFillStyle(0)
+    legend.SetBorderSize(0)
+
+    legend.Draw()
+
+    #Draw extra text
+    DrawExtraText(plotpad, additionalInfo)
+
+    #Return to canvas
+    Canv.cd()
+
+    #Second pad
+    ratiopad = TPad("ratiopad", "ratiopad", 0, 0.05, 1, .3)
+    ratiopad.SetTopMargin(0.05)
+    ratiopad.SetBottomMargin(0.25)
+    ratiopad.Draw()
+    ratiopad.cd()
+   
+    ratio = observed.Clone('ratio')
+    ratio.Divide(predicted)
+
+    #Set Style for bottom plot
+    ratio.SetTitle(";" + xtitle + "; Obs./pred.")
+    ratio.GetXaxis().SetTitleSize(.12)
+    ratio.GetYaxis().SetTitleSize(.12)
+    ratio.GetYaxis().SetTitleOffset(.6)
+    ratio.GetXaxis().SetLabelSize(.12)
+    ratio.GetYaxis().SetLabelSize(.12)
+    ratio.SetMinimum(0.5)
+    ratio.SetMaximum(1.5)
+    ratio.Draw("EP")
+
+    #Draw a guide for the eye
+    line = TLine(ratio.GetXaxis().GetXmin(),1,ratio.GetXaxis().GetXmax(),1)
+    line.SetLineColor(ROOT.kRed)
+    line.SetLineWidth(1)
+    line.SetLineStyle(1)
+    line.Draw("Same")
+   
+    #Throw CMs lumi at it
+    cl.CMS_lumi(Canv, 4, 11, 'Preliminary', True)
+ 
+    #Save everything
+    savePlots(Canv, destination)
 
 def plotROC(xdata, ydata, xlabel, ylabel, legendNames,destination, xerror = None, yerror = None, xlog = False, ylog = False, additionalInformation = None):
     GeneralSettings()
@@ -255,7 +417,7 @@ def plotROC(xdata, ydata, xlabel, ylabel, legendNames,destination, xerror = None
     mgraph.SetTitle(";" + xlabel + ";" + ylabel)
     
     cl.CMS_lumi(Canv, 4, 11, 'Simulation', False)
-    
+ 
     if xlog :
         Canv.SetLogx()
         mgraph.GetXaxis().SetRangeUser(0.3*GetNestedMin(xdata), 30*GetNestedMax(xdata))
@@ -308,10 +470,96 @@ def plotROC(xdata, ydata, xlabel, ylabel, legendNames,destination, xerror = None
         legend.SetBorderSize(0)
         legend.Draw() 
 
-    #Save
-    Canv.SaveAs(destination + ".pdf")    
-    Canv.SaveAs(destination + ".png")    
-    Canv.SaveAs(destination + ".root")    
+    #Save everything
+    savePlots(Canv, destination)
+
+def plotROCfromgraph(graphs, xlabel, ylabel, legendNames,destination, xlog = False, ylog = False, additionalInformation = None):
+    GeneralSettings()
+    #Make sure single curves also pass
+    try:
+        len(graphs)
+    except:
+        graphs = [graphs]    
+
+    #Create Canvas
+    Canv = TCanvas("Canv"+destination, "Canv"+destination, 1000, 1000)
+    
+    tdr.setTDRStyle()
+    
+    #Create TGraph
+    mgraph = TMultiGraph()
+    
+    for i, graph in enumerate(graphs):
+        graph.SetMarkerSize(1.5)
+        graph.SetLineColor(TColor.GetColor(GetLineColor(graphs.index(graph))))
+        graph.SetMarkerColor(TColor.GetColor(GetLineColor(graphs.index(graph))))
+        graph.SetMarkerStyle(GetMarker(graphs.index(graph)))
+        mgraph.Add(graph)
+
+    mgraph.Draw("APLine")
+    mgraph.SetTitle(";" + xlabel + ";" + ylabel)
+    
+    cl.CMS_lumi(Canv, 4, 11, 'Simulation', False)
+    
+    xmax = GetXMax(graphs)
+    xmin = GetXMin(graphs)
+    ymax = GetYMax(graphs)
+    ymin = GetYMin(graphs)
+
+    if xlog :
+        Canv.SetLogx()
+        mgraph.GetXaxis().SetRangeUser(0.3*xmin, 30*xmax)
+    else :
+        mgraph.GetXaxis().SetRangeUser(0.7*xmin, 1.3*xmax)
+    
+    if ylog :
+        Canv.SetLogy()
+        mgraph.GetYaxis().SetRangeUser(0.3*ymin, 10*ymax)
+    else :
+        mgraph.GetYaxis().SetRangeUser(0.5*ymin, 1.2*ymax)
+   
+    #Write extra text
+    if additionalInformation is not None:
+        lastYpos = 0.8
+        lastCorrectedYpos = None
+        extraText = TLatex()
+        for info in additionalInformation:
+            try :
+                extraTextString = info[0]
+                extraTextXpos = info[1]
+                extraTextYpos = info[2]
+                extraTextSize = info[3]
+            except:
+                print("Wrong Format for additionalInformation. Stopping")
+                pass
+            
+            if extraTextSize is None:
+                extraTextSize = 0.03
+            if extraTextXpos is None:
+                extraTextXpos = 0.2
+            if extraTextYpos is None:
+                if lastYpos is None:
+                    extraTextYpos = lastCorrectedYpos -0.05 
+                else: extraTextYpos = 0.8
+            
+            extraText.SetNDC()
+            extraText.SetTextAlign(12)
+            extraText.SetTextSize(extraTextSize)
+            extraText.DrawLatex(extraTextXpos, extraTextYpos, extraTextString)
+            
+            lastYpos = info[2]
+            lastCorrectedYpos = extraTextYpos
+
+    if legendNames:
+        legend = TLegend(0.7, .7, .9, .9)
+        for g, n in zip(graphs, legendNames):
+            legend.AddEntry(g, n)
+        legend.SetFillStyle(0)
+        legend.SetBorderSize(0)
+        legend.Draw() 
+
+    #Save everything
+    savePlots(Canv, destination)
 
 def DrawHist(hist, xlabel, ylabel, legendNames, destination, ylog = False):
     GeneralSettings()
@@ -339,9 +587,9 @@ def DrawHist(hist, xlabel, ylabel, legendNames, destination, ylog = False):
     #Start Drawing
     for h in hist:
         if(hist.index(h) == 0) :
-            h.Draw("EHIST")
+            h.Draw("HIST")
         else:
-            h.Draw("EHISTSAME")
+            h.Draw("HISTSAME")
     #Create Legend
     if legendNames :
         legend = TLegend(0.7, .7, .9, .9)
@@ -354,10 +602,8 @@ def DrawHist(hist, xlabel, ylabel, legendNames, destination, ylog = False):
     #CMS lumi
     cl.CMS_lumi(Canv, 4, 11, 'Simulation', False)
 
-    #Save
-    Canv.SaveAs(destination + ".pdf")    
-    Canv.SaveAs(destination + ".png")    
-    Canv.SaveAs(destination + ".root")
+    #Save everything
+    savePlots(Canv, destination)
 
 def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName, destination, ylog = False, customLabels = None, extraText = None, scaleSignalToBkgr = False, DivideByLine = None):
     GeneralSettings()
@@ -367,6 +613,8 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
         BkgrHist = [BkgrHist]
     if not isinstance(SignalHist,(list,)):
         SignalHist = [SignalHist]
+
+    BkgrHist, legendNames = orderHist(BkgrHist, legendNames)
 
     #Add all backgrounds
     totBkgr = BkgrHist[0].Clone("TotBkgr")
@@ -382,9 +630,9 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
     Canv = TCanvas("Canv"+destination, "Canv"+destination, 1500, 1000)
 
     #Set Histogram Styles
-    for i, h in enumerate(BkgrHist) :
-        h.SetFillColor(TColor.GetColor(GetStackColor(i)))
-        h.SetLineColor(TColor.GetColor(GetStackColor(i)))
+    for h, n in zip(BkgrHist, legendNames):
+        h.SetFillColor(TColor.GetColor(GetStackColorTauPOGbyName(n)))
+        h.SetLineColor(TColor.GetColor(GetStackColorTauPOGbyName(n)))
 
     for i, h in enumerate(SignalHist):
         h.SetMarkerColor(TColor.GetColor(GetLineColor(i)))
@@ -392,14 +640,15 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
         h.SetMarkerStyle(GetMarker(i))
 
     list_of_significance_hists = []
-    
     for i, sh in enumerate(SignalHist):
         #Calculate significance
         total = totBkgr.Clone("Total")
         total.Add(sh)
         sqrt_total = total.Clone('Sqrt_Total')
         for xbin in xrange(1, total.GetSize()-1):                     #GetSize returns nbins + 2 (for overflow and underflow bin)
-            sqrt_total.SetBinContent(xbin, np.sqrt(total.GetBinContent(xbin)))
+            sqrt_x = np.sqrt(total.GetBinContent(xbin))
+            sqrt_total.SetBinContent(xbin, sqrt_x)
+            sqrt_total.SetBinError(xbin, 0.5*total.GetBinError(xbin)/sqrt_x)
         
         significance = sh.Clone('Signal'+str(i))
         significance.Divide(sqrt_total)
@@ -420,15 +669,15 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
     hs.SetTitle(title)
 #    hs.GetHistogram().GetXaxis().SetTickLength(0)
     hs.GetHistogram().GetXaxis().SetLabelOffset(9999999)
-    hs.GetHistogram().SetMaximum(1)
+    #hs.GetHistogram().SetMaximum(1)
     #Set range
-    overallMin = GetOverallMinimum(BkgrHist+SignalHist)
+    overallMin = GetOverallMinimum(BkgrHist+SignalHist, True)
     overallMax = GetOverallMaximum([totBkgr]+SignalHist)
   
      
     if ylog :
         if overallMin == 0.:
-            overallMin = 0.5
+            overallMin = 0.1
         ymin = 0.3*overallMin
         ymax = 30*overallMax
         plotpad.SetLogy()
@@ -492,10 +741,19 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
     ratiopad.Draw()
     ratiopad.cd()
 
+    #print list_of_significance_hists[0].GetMaximum(), list_of_significance_hists[1].GetMaximum(), list_of_significance_hists[2].GetMaximum()
     overallMin = GetOverallMinimum(list_of_significance_hists)
     overallMax = GetOverallMaximum(list_of_significance_hists)
 
     significance = list_of_significance_hists[0]
+    
+    #Prepare lines before changing maximum
+    lines_bottom = []
+    for i, sig in enumerate(list_of_significance_hists):
+        lines_bottom.append(TLine(sig.GetXaxis().GetXmin(), sig.GetMaximum(),sig.GetXaxis().GetXmax() , sig.GetMaximum()))
+        lines_bottom[i].SetLineColor(TColor.GetColor(GetLineColor(i)))
+        lines_bottom[i].SetLineStyle(3)
+
     #Set Style for bottom plot
     significance.SetTitle(";" + xtitle + "; S/#sqrt{S+B}")
     significance.GetXaxis().SetTitleSize(.12)
@@ -504,11 +762,14 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
     significance.GetXaxis().SetLabelSize(.12)
     significance.GetYaxis().SetLabelSize(.12)
     significance.SetMinimum(0.)
-    significance.SetMaximum(0.3*overallMax)
+    significance.SetMaximum(1.3*overallMax)
     significance.Draw("EP")
     
     for sig in list_of_significance_hists[1:]:
         sig.Draw('EPSame')
+    
+    for line in lines_bottom:
+        line.Draw("same")
 
     #Set custom x labels
     xaxis = significance.GetXaxis()
@@ -526,16 +787,37 @@ def calcAndDrawSignificance(SignalHist, BkgrHist, xtitle, legendNames, DataName,
             for i, label  in zip(range(number_of_bins), customLabels):
                 xaxis.SetBinLabel(i+1, label)  
   
-                    
- 
     #Throw CMs lumi at it
     cl.CMS_lumi(Canv, 4, 11, 'Simulation Preliminary', True)
 
     #Save everything
-    Canv.SaveAs(destination + ".pdf")
-    Canv.SaveAs(destination + ".png")
-    Canv.SaveAs(destination + ".root")
-   
+    savePlots(Canv, destination)
+    ROOT.SetOwnership(Canv,False)               #https://root-forum.cern.ch/t/tlatex-crashing-in-pyroot-after-many-uses/21638/4
+    return
+
+def draw2DHist(h, xlabel, ylabel, output):
+    
+    tdr.setTDRStyle()
+    GeneralSettings("4.3f")
+    gStyle.SetPalette(ROOT.kIsland)
+    gStyle.SetPadRightMargin(0.15)
+     
+    #Create Canvas
+    Canv = TCanvas("Canv"+output, "Canv"+output, 1000, 1000)
+
+    h.SetTitle(';'+xlabel+';'+ylabel) 
+    h.Draw('ETextColz')
+        
+    #Throw CMs lumi at it
+    cl.CMS_lumi(Canv, 4, 0, 'Preliminary', True)
+
+    #Save everything
+    savePlots(Canv, output)
 
     ROOT.SetOwnership(Canv,False)               #https://root-forum.cern.ch/t/tlatex-crashing-in-pyroot-after-many-uses/21638/4
     return
+
+    
+
+
+
