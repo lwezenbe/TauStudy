18a7a77ce0010fd9b91ab6b23d1c696739473d65
Liam <liamwezenbeek@hotmail.com>
2019-07-19 11:22:42 +0200

    Changed proxy location

--- scripts/runOnCream02.sh
+++ scripts/runOnCream02.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PYTHONUNBUFFERED=TRUE
-cd /user/lwezenbe/private/CMSSW_9_4_10/src
 source $VO_CMS_SW_DIR/cmsset_default.sh
+cd /user/lwezenbe/private/CMSSW_10_2_13/src
 eval `scram runtime -sh`
+export X509_USER_PROXY=/user/$USER/x509up_u$(id -u $USER) 
 eval $command
